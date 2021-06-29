efeea98dd22f508234d333e63d989ca929516639
Thibaut <thibaut.mattio@gmail.com>
2021-06-29 13:01:22 +0200

    Use dune instead of make

--- template/script/watch.sh
+++ template/script/watch.sh
@@ -14,7 +14,7 @@ function sigint_handler() {
 trap sigint_handler SIGINT
 
 while true; do
-  make build
+  dune build
   $cmd &
   fswatch -r -1 $source_dirs
   printf "\nRestarting server.exe due to filesystem change\n"
